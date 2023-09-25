e8124b2cd7210da228c82aa38eac60c174ad11cb
Martin Honnen <martin.honnen@gmx.de>
2023-09-25 12:37:24 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import net.sf.saxon.jaxp.TransformerImpl;
+import net.sf.saxon.s9api.MessageListener2;
+import net.sf.saxon.s9api.QName;
+import net.sf.saxon.s9api.XdmNode;
+
+import javax.xml.transform.*;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+public class Main {
+    boolean terminated = false;
+    String terminationMessage;
+
+    static TransformerFactory transformerFactory = TransformerFactory.newInstance();
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.transform("sample2.xml", "sheet1.xsl");
+        System.out.println();
+        main.transform("sample1.xml", "sheet1.xsl");
+
+    }
+
+    public void transform(String xmlSource, String xsltSource) {
+        try {
+            Transformer xsltProcessor = transformerFactory.newTransformer(new StreamSource(xsltSource));
+
+            ((TransformerImpl)xsltProcessor).getUnderlyingXsltTransformer().setMessageHandler(message -> {
+                if (message.isTerminate()) {
+                    terminated = true;
+                    terminationMessage = message.getStringValue();
+                }
+            });
+
+            xsltProcessor.transform(new StreamSource(xmlSource), new StreamResult(System.out));
+
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            System.out.println(e.getMessageAndLocation());
+            if (terminated)
+                System.out.println(terminationMessage);
+        }
+    }
+}
